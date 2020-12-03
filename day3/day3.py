276418072cb5299734b4035a024630892324626f
Leiv Erik Rosseland <leiv-erik.rosseland@spk.no>
2020-12-03 09:04:08 +0100

    day 3 (refaktor)

--- day3/day3.py
+++ day3/day3.py
@@ -2,59 +2,31 @@ import timing
 import os
 import sys
 
-def part1():
+map = {}
+x_max = y_max = y = 0
 
-    x_max = 0
-    y_max = 0
+with open(os.path.join(sys.path[0], "input_day3.txt"), "r") as file:
+    for line in file:
+        x=0
+        for char in line.strip():
+            map[(x,y)] = char
+            x_max = x = x + 1
+        y_max = y = y + 1
 
-    map = {}
+trees_1 = trees_2 = trees_3 = trees_4 = trees_5 = 0
+for xy in range(1,y_max):
+    if map[(xy % x_max,xy)] == '#':
+        trees_1 += 1
+    if map[((xy * 3) % x_max,xy)] == '#':
+        trees_2 += 1
+    if map[((xy * 5) % x_max,xy)] == '#':
+        trees_3 += 1
+    if map[((xy * 7) % x_max,xy)] == '#':
+        trees_4 += 1
+    if xy % 2 == 0:
+        if map[((xy/2) % x_max,xy)] == '#':
+            trees_5 += 1
 
-    x = 0
-    y = 0
+print("Solution 1: ", trees_2)
 
-
-    with open(os.path.join(sys.path[0], "input_day3.txt"), "r") as file:
-        for line in file:
-            for char in line.strip():
-                map[(x,y)] = char
-                x+=1
-                if x > x_max:
-                    x_max = x
-            x=0
-            y+=1
-            if y > y_max:
-                y_max = y
-
-    trees_1 = 0
-    trees_2 = 0
-    trees_3 = 0
-    trees_4 = 0
-    trees_5 = 0
-    x_1 = 0
-    x_2 = 0
-    x_3 = 0
-    x_4 = 0
-    x_5 = 0
-    for y in range(1,y_max):
-        x_1 = (x_1+1) % x_max
-        if map[(x_1,y)] == '#':
-            trees_1 += 1
-        x_2 = (x_2+3) % x_max
-        if map[(x_2,y)] == '#':
-            trees_2 += 1
-        x_3 = (x_3+5) % x_max
-        if map[(x_3,y)] == '#':
-            trees_3 += 1
-        x_4 = (x_4+7) % x_max
-        if map[(x_4,y)] == '#':
-            trees_4 += 1
-        if y % 2 == 0:
-            x_5 = (x_5+1) % x_max
-            if map[(x_5,y)] == '#':
-                trees_5 += 1
-
-    print("Solution 1: ", trees_2)
-
-    print("solution 2: ", trees_1 * trees_2 * trees_3 * trees_4 * trees_5)
-
-part1()
+print("solution 2: ", trees_1 * trees_2 * trees_3 * trees_4 * trees_5)
