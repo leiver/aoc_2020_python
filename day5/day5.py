f050360cc2b77c5fff81bb56a6b088957c543f65
Leiv Erik Rosseland <leiv-erik.rosseland@knowit.no>
2020-12-05 19:57:53 +0100

    completed and optimized day 5

--- day5/day5.py
+++ day5/day5.py
@@ -3,6 +3,9 @@ import sys
 
 highest_id = 0
 seat_map = {}
+almost_solution = {}
+solutions = {}
+
 with open(os.path.join(sys.path[0], "input_day5.txt"), "r") as file:
     for line in file:
         line = line.strip()
@@ -17,25 +20,31 @@ with open(os.path.join(sys.path[0], "input_day5.txt"), "r") as file:
             if index < len(column_code) and column_code[index] == "R":
                 column_value += pow(2, index)
 
-        if 0 < row_value < 127 and 0 < column_value < 7:
-            # print("added to seatmap " + str(row_value) + ", " + str(column_value))
-            seat_map[(row_value, column_value)] = 1
-
         seat_id = (row_value * 8) + column_value
+
+        seat_map[seat_id] = 0
+        if seat_id in almost_solution:
+            almost_solution.pop(seat_id)
+        if seat_id in solutions:
+            solutions.pop(seat_id)
+
+        if seat_id - 1 not in seat_map:
+            if seat_id - 1 not in almost_solution:
+                almost_solution[seat_id - 1] = 1
+            else:
+                solutions[seat_id - 1] = 2
+
+        if seat_id + 1 not in seat_map:
+            if seat_id + 1 not in almost_solution:
+                almost_solution[seat_id + 1] = 1
+            else:
+                solutions[seat_id + 1] = 2
+
+        seat_map[seat_id] = 0
         if seat_id > highest_id:
             highest_id = seat_id
 
 print("solution part1: " + str(highest_id))
 
-for row in range(8,117):
-    for column in range():
-        if (row, column) not in seat_map.keys():
-            print("solution part2: " + str((row * 8) + column) + " at: " + str(row) + ", " + str(column))
+print("solution part2: " + str(list(solutions.keys())[0]))
 
-
-
-
-
-# BFFFBBF RRR: row 70, column 7, seat ID 567.
-# FFFBBBF RRR: row 14, column 7, seat ID 119.
-# BBFFBBF RLL: row 102, column 4, seat ID 820.
